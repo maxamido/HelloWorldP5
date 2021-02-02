5e59faaabb5d56b8fc95bd08951c8b1149c5ce4a
Max Amido <78068799+maxamido@users.noreply.github.com>
2021-02-01 22:21:20 -0800

    Added Hello World Files

---
+++ sketch.js
@@ -0,0 +1,15 @@
+
+// Setup code goes here
+function setup() {
+  createCanvas(1000, 1200);
+  textSize(40);
+  textAlign(CENTER);
+ }
+
+
+// Draw code goes here
+function draw() {
+  background(15,125,99);
+  fill(250);
+  text("Ello, Earl", width/2, height/2);
+}
